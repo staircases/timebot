9a79cf236572b3d5103ddfb59ee6bf2e7cec9155
Xavier Calabia <xaviermbcalabia@gmail.com>
2022-07-08 12:37:29 +0800

    Update deploy-commands.js
    Removed unused guildId

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, token } = require('./config.json');
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
