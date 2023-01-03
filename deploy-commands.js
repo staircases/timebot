7d01d13b6c0310d3fb747b2a531f137ab7705fd4
staircases <xaviermbcalabia@gmail.com>
2023-01-03 13:10:01 +0800

    update deploy-commands to match master version

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, token } = require('./config.json');
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
@@ -17,6 +17,6 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
