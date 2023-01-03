b713364247de723689cef9883ba44bcba8eaba54
staircases <xaviermbcalabia@gmail.com>
2023-01-03 19:32:25 +0800

    added formatting checks to whenis

--- commands/whenis.js
+++ commands/whenis.js
@@ -17,15 +17,27 @@ module.exports = {
 			option.setName('local timezone')
 				.setDescription('Your local timezone as hours ahead or behind UTC. Defaults to 0.')
 				.setRequired(false)),
-		async execute(interaction) {
+	async execute(interaction) {
+		const timeString = interaction.options.getString('time');
+		const fTimezone = interaction.options.getString('foreign timezone') ?? '+0:00';
+		const lTimezone = interaction.options.getString('local timezone') ?? '+0:00';
 
-			const timeString = interaction.options.getString('time');
-			const fTimezone = interaction.options.getString('foreign timezone') ?? '+0:00';
-			const lTimezone = interaction.options.getString('local timezone') ?? '+0:00';
+		const [hours, minutes] = checkTimeFormat(timeString);
+		const [fTimezoneHours, fTimezoneMinutes] = checkTimeZoneFormat(fTimezone);
+		const [lTimezoneHours, lTimezoneMinutes] = checkTimeZoneFormat(lTimezone);
 
-			const [hours, minutes] = checkTimeFormat(timeString);
-			const [fTimezoneHours, fTimezoneMinutes] = checkTimeZoneFormat(fTimezone);
-			const [lTimezoneHours, lTimezoneMinutes] = checkTimeZoneFormat(lTimezone);
+		if (!hours && hours !== 0) {
+		interaction.reply(`Your time is formatted incorrectly! Format it as \`XX:XX AM/PM\`.`);
+		return;
+		}
+		if (!fTimezoneHours && fTimezoneHours !== 0) {
+			interaction.reply(`Your foreign timezone is formatted incorreectly! Format it as \`+/-XX:XX\`.`);
+			return;
+		}
+		if (!lTimezoneHours && lTimezoneHours !== 0) {
+			interaction.reply(`Your local timezone is formatted incorreectly! Format it as \`+/-XX:XX\`.`);
+			return;
+		}
 
 		},
 };
