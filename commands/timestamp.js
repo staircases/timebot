18f82748647b1458956f8a2186f4f1fc7325e659
staircases <xaviermbcalabia@gmail.com>
2023-01-03 16:24:34 +0800

    Update timestamp.js

--- commands/timestamp.js
+++ commands/timestamp.js
@@ -1,39 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
-const { checkTimeFormat, checkTimeZoneFormat } = require('../helper_modules/timehelper.js')
-
-function checkTimeFormat (time) {
-	const regexp = /(\d{1,2}):(\d{2})\s?([AP]M)?/gi;
-	const matched = [...time.matchAll(regexp)];
-
-	if (matched.length == 0) {
-		return [false, false];
-	} else {
-		const hours = Math.min(parseInt(matched[0][1]), 23);
-		const minutes = Math.min(parseInt(matched[0][2]), 59);
-
-		if (matched[0][3] === undefined || matched[0][3].toUpperCase() == 'AM') {
-			return [hours, minutes];
-		} else if (matched[0][3].toUpperCase() == 'PM') {
-			return [Math.min(hours + 12, 24), minutes];
-		}
-		
-	}
-};
-
-function checkTimeZoneFormat (timezone) {
-	const regexp = /([+-])(\d{1,2}):(\d{2})/gi;
-	const matched = [...timezone.matchAll(regexp)];
-
-	if (matched.length == 0) {
-		return [false, false];
-	} else {
-		const sign = (matched[0][1] == "+") ? 1 : -1;
-		const hours = Math.min(parseInt(matched[0][2]), 23);
-		const minutes = Math.min(parseInt(matched[0][3]), 59);
-		return [hours * sign, minutes];
-	}
-};
+const { checkTimeFormat, checkTimeZoneFormat } = require('../helper_modules/timehelper.js');
 
 function parseTime (time) {
 
