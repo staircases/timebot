1117d32c1eddda1cf34208f9736ea583359b8ccc
staircases <xaviermbcalabia@gmail.com>
2023-01-04 16:56:45 +0800

    update index.js and events to discord.js v14

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,20 @@
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
-		if (!interaction.isCommand()) return;
+		if (!interaction.isChatInputCommand()) return;
 
 		const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) return;
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
 
 		try {
 			await command.execute(interaction);
 		} catch (error) {
+			console.error(`Error executing ${interaction.commandName}`);
 			console.error(error);
-			await interaction.reply({ content: 'we did a fucky wucky ligma balls', ephemeral: true });
 		}
-	}
+	},
 };
