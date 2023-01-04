df2a23d9a55b50a22e4b0e6b886e4165ed493b4b
staircases <xaviermbcalabia@gmail.com>
2023-01-04 17:00:39 +0800

    readded missing command lines

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
